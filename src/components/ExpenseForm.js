005a4044d5f7a778044114adb96a15638ffe9f32
Peter Kim <peter.minho.kim@gmail.com>
2020-09-21 18:29:58 -0400

    Filter by dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import moduleName from 'react-dates/lib/css/_datepicker.css';
 
 class ExpenseForm extends React.Component {
   state = {
-    description: '',
-    note: '',
-    amount: ''
+    description: this.props.expense ? this.props.expense.description : '',
+    note: this.props.expense ? this.props.expense.note : '',
+    amount: this.props.expense
+      ? (this.props.expense.amount / 100).toString()
+      : '',
+    createdAt: this.props.expense
+      ? moment(this.props.expense.createdAt)
+      : moment(),
+    calendarFocused: false,
+    error: ''
+  };
+
+  onDateChange = (createdAt) => {
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
   };
 
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
@@ -24,8 +61,17 @@ class ExpenseForm extends React.Component {
             value={this.state.amount}
             onChange={(e) => {
               const amount = e.target.value;
-              if (amount.match(/^\d*(\.\d{0,2})?$/)) this.setState({ amount });
+              if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/))
+                this.setState({ amount });
             }}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={(day) => false}
           />
           <textarea
             placeholder="Add a note for your expense"
