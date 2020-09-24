9f91cc2ad184e50f207e4653a792119367124a63
Peter Kim <peter.minho.kim@gmail.com>
2020-09-24 10:26:35 -0400

    Test EditExpensePage

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
